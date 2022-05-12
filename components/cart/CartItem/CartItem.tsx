e853313e31b0d6bb04e230dc21d6b5c1eb213826
Karmotrine <yuanrcurena@gmail.com>
2022-05-12 18:29:15 +0800

    Pruned unnecessary components

--- components/cart/CartItem/CartItem.tsx
+++
@@ -1,7 +0,0 @@
-import { ChangeEvent, FocusEventHandler, useEffect, useState } from 'react'
-import cn from 'clsx'
-import Image from 'next/image'
-import Link from 'next/link'
-//import s
-import { useUI } from "../../context"
-import { useCart }
