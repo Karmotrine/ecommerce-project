938d7bb605666694af67da896fe64577f8deab17
Karmotrine <yuanrcurena@gmail.com>
2022-05-18 09:53:01 +0800

    Adjust signInSuccessUrl to current url

--- components/auth/LoginView.tsx
+++ components/auth/LoginView.tsx
@@ -4,18 +4,22 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { app as firebaseClient, auth } from "../../lib/firebaseClient"
 import { Modal, Text, Button } from "@mantine/core"
-
-const uiConfig = {
-    // Popup signin flow rather than redirect flow.
-    signInFlow: 'popup',
-    // Redirect to /signedIn after sign in is successful.
-    // Alternatively you can provide a callbacks.signInSuccess function.
-    signInSuccessUrl: "/",
-    // Only Google as auth provider
-    signInOptions: [GoogleAuthProvider.PROVIDER_ID],
-}
+import { useRouter } from 'next/router';
 
 export default function LoginView() {
+    const { asPath } = useRouter()
+
+    const uiConfig = {
+        // Popup signin flow rather than redirect flow.
+        signInFlow: 'popup',
+        // Redirect to /signedIn after sign in is successful.
+        // Alternatively you can provide a callbacks.signInSuccess function.
+        signInSuccessUrl: asPath,
+        // Only Google as auth provider
+        signInOptions: [GoogleAuthProvider.PROVIDER_ID],
+    }
+    
+
     const authInstance = getAuth(firebaseClient)
     const [ user, loading, error ] = useAuthState(authInstance)
     const [ modalOpen, setModalOpen ] = useState(false);
