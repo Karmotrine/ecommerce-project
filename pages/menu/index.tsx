3cb976f9ee8ae7f736792d6d22c59275d039a9e6
Yuan <72153098+Karmotrine@users.noreply.github.com>
2022-06-14 19:24:59 +0800

    Update index.tsx (#9)
    Menu updates without pictures
    
    Co-authored-by: Poxero99 <82596499+Poxero99@users.noreply.github.com>

--- pages/menu/index.tsx
+++ pages/menu/index.tsx
@@ -1,4 +1,4 @@
-import { Container, Text, Divider, createStyles, Center } from "@mantine/core";
+import { Container, Text, Divider, createStyles, Center, Box} from "@mantine/core";
 
 const useStyles = createStyles((theme) => ({
     headerFont: {
@@ -16,7 +16,221 @@ export default function Menu() {
                     <Text className={classes.headerFont}>Menu</Text>
                 </Center>
                 <Divider />
-                <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras bibendum facilisis sodales. Maecenas eu luctus nulla. Nunc id purus ac tellus gravida semper at ut mauris. Curabitur non lacus mattis, dapibus ex blandit, pulvinar lacus. Duis tempor nisl luctus nisi lobortis lobortis. Cras blandit feugiat tellus, et luctus lectus pharetra vel. Ut dui eros, tincidunt at massa ac, feugiat convallis ipsum. Proin interdum arcu nibh, sit amet gravida ipsum viverra in. Nunc elementum rutrum sem, a interdum lacus aliquet quis. Curabitur nibh enim, pharetra ac urna ut, auctor tincidunt orci. Donec a aliquam ligula.</Text>
+                
+                <Text weight={700}>RAMEN KITS</Text>
+                
+                <Boxsx={(theme) => ({
+                 backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+                 textAlign: 'left',
+                 padding: theme.spacing.xl,
+                 borderRadius: theme.radius.md,
+                 cursor: 'pointer','&:hover': {
+                 backgroundColor:
+            theme.colorScheme === 'light' ? theme.colors.white[2] : theme.colors.gray[5],
+        },
+      })}>
+                
+                <div style={{ width: 240, marginLeft: 'auto', marginRight: 'auto' }}>
+                 <Image radius="md" src="" alt="Box with Chopsticks"/>
+                </div>
+                    <list.Item trigger ="hover" delay={500} control = {<Button> Add to Cart </Button>}> 
+                        <Text weight={500}>Maruzen Ramen Kit</Text>
+                        <Text> Each Maruzen Ramen Kit contains: </Text>
+                        <List.Item> 300g Ramen Noodles Pasta </List.Item>
+                        <List.Item> 150g Ramen Paste </List.Item>
+                        <List.Item> Nori Sheets </List.Item>
+                        <Text> ₱320.00 </Text>
+                    </List.Item>
+                
+                <div style={{ width: 240, marginLeft: 'auto', marginRight: 'auto' }}>
+                 <Image radius="md" src="" alt="Box with Chopsticks"/>
+                </div>
+                    <list.Item trigger ="hover" delay={500} control = {<Button> Add to Cart </Button>}> 
+                        <Text weight={500}>Fukuoka Strong Taste Ramen Kit</Text>
+                        <Text> Each Fukuoka Strong Taste Ramen Kit contains: </Text>
+                        <List.Item> Nori Sheets </List.Item>
+                        <List.Item> Ramen Noodles </List.Item>
+                        <List.Item> It is good for 6 to 8 servings too! </List.Item>
+                        <List.Item> Add exciting toppings like chasu, egg, chili, etc! </List.Item>
+                        <Text> ₱320.00 </Text>
+                    </List.Item>
+                
+                <div style={{ width: 240, marginLeft: 'auto', marginRight: 'auto' }}>
+                 <Image radius="md" src="" alt="Box with Chopsticks"/>
+                </div>
+                    <list.Item trigger ="hover" delay={500} control = {<Button> Add to Cart </Button>}> 
+                        <Text weight={500}>Tonkotsu Ramen Kit</Text>
+                        <Text> Each Tonkotsu Ramen Kit contains: </Text>
+                        <List.Item> 150g Ramen Paste </List.Item>
+                        <List.Item> 300g Ramen Noodles </List.Item>
+                        <List.Item> Nori Sheets </List.Item>
+                        <Text> ₱320.00 </Text>
+                    </List.Item>
+                    
+                    
+                    <Divider /> 
+                    <Text weight={700}>BENTO</Text>
+                
+                <Boxsx={(theme) => ({
+                 backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+                 textAlign: 'right',
+                 padding: theme.spacing.xl,
+                 borderRadius: theme.radius.md,
+                 cursor: 'pointer','&:hover': {
+                 backgroundColor:
+            theme.colorScheme === 'light' ? theme.colors.white[2] : theme.colors.gray[5],
+        },
+      })}>
+                
+                <div style={{ width: 240, marginLeft: 'auto', marginRight: 'auto' }}>
+                 <Image radius="md" src="" alt="Box with Chopsticks"/>
+                </div>
+                    <list.Item trigger ="hover" delay={500} control = {<Button> Add to Cart </Button>}> 
+                        <Text weight={500}>Bento 1: Sakana Fry Kit</Text>
+                        <Text> ₱295.00 </Text>
+                    </List.Item>
+                
+                <div style={{ width: 240, marginLeft: 'auto', marginRight: 'auto' }}>
+                 <Image radius="md" src="" alt="Box with Chopsticks"/>
+                </div>
+                    <list.Item trigger ="hover" delay={500} control = {<Button> Add to Cart </Button>}> 
+                        <Text weight={500}>Bento 2: Yaki Tori</Text>
+                        <Text> ₱380.00 </Text>
+                        
+                    </List.Item>
+                
+                <div style={{ width: 240, marginLeft: 'auto', marginRight: 'auto' }}>
+                 <Image radius="md" src="" alt="Box with Chopsticks"/>
+                </div>
+                    <list.Item trigger ="hover" delay={500} control = {<Button> Add to Cart </Button>}> 
+                        <Text weight={500}>Bento 3: Tonkatsu </Text>
+                        <Text> ₱315.00 </Text>
+                    </List.Item>
+                    
+                    <div style={{ width: 240, marginLeft: 'auto', marginRight: 'auto' }}>
+                 <Image radius="md" src="" alt="Box with Chopsticks"/>
+                </div>
+                    <list.Item trigger ="hover" delay={500} control = {<Button> Add to Cart </Button>}> 
+                        <Text weight={500}>Bento 4: Chicken Teriyaki </Text>
+                        <Text> ₱380.00 </Text>
+                    </List.Item>
+                    
+                    <div style={{ width: 240, marginLeft: 'auto', marginRight: 'auto' }}>
+                 <Image radius="md" src="" alt="Box with Chopsticks"/>
+                </div>
+                    <list.Item trigger ="hover" delay={500} control = {<Button> Add to Cart </Button>}> 
+                        <Text weight={500}>Bento 5: Ebi Tempura </Text>
+                        <Text> ₱400.00 </Text>
+                    </List.Item>
+                    
+                    <div style={{ width: 240, marginLeft: 'auto', marginRight: 'auto' }}>
+                 <Image radius="md" src="" alt="Box with Chopsticks"/>
+                </div>
+                    <list.Item trigger ="hover" delay={500} control = {<Button> Add to Cart </Button>}> 
+                        <Text weight={500}>Bento 6: Gyu Hirenku </Text>
+                        <Text> ₱610.00 </Text>
+                    </List.Item>
+                    
+                    
+                    <Divider /> 
+                    <Text weight={700}>EXTRA AND DRINKS</Text>
+                
+                <Boxsx={(theme) => ({
+                 backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+                 textAlign: 'center',
+                 padding: theme.spacing.xl,
+                 borderRadius: theme.radius.md,
+                 cursor: 'pointer','&:hover': {
+                 backgroundColor:
+            theme.colorScheme === 'light' ? theme.colors.white[2] : theme.colors.gray[5],
+        },
+      })}>
+                
+                <div style={{ width: 240, marginLeft: 'auto', marginRight: 'auto' }}>
+                 <Image radius="md" src="" alt="Box with Chopsticks"/>
+                </div>
+                    <list.Item trigger ="hover" delay={500} control = {<Button> Add to Cart </Button>}> 
+                        <Text weight={500}> Ice Tea </Text>
+                        <List.Item> Regular </List.Item>
+                        <List.Item> Large </List.Item>
+                        <Text> ₱20.00 </Text>
+                    </List.Item>
+                
+                <div style={{ width: 240, marginLeft: 'auto', marginRight: 'auto' }}>
+                 <Image radius="md" src="" alt="Box with Chopsticks"/>
+                </div>
+                    <list.Item trigger ="hover" delay={500} control = {<Button> Add to Cart </Button>}> 
+                        <Text weight={500}> Matcha </Text>
+                        <List.Item> Regular </List.Item>
+                        <List.Item> Large </List.Item>
+                        <Text> ₱100.00 </Text>
+                        
+                    </List.Item>
+                
+                <div style={{ width: 240, marginLeft: 'auto', marginRight: 'auto' }}>
+                 <Image radius="md" src="" alt="Box with Chopsticks"/>
+                </div>
+                    <list.Item trigger ="hover" delay={500} control = {<Button> Add to Cart </Button>}> 
+                        <Text weight={500}> Ice Coffee </Text>
+                        <List.Item> Regular </List.Item>
+                        <List.Item> Large </List.Item>
+                        <Text> ₱30.00 </Text>
+                    </List.Item>
+                    
+                    <div style={{ width: 240, marginLeft: 'auto', marginRight: 'auto' }}>
+                 <Image radius="md" src="" alt="Box with Chopsticks"/>
+                </div>
+                    <list.Item trigger ="hover" delay={500} control = {<Button> Add to Cart </Button>}> 
+                        <Text weight={500}> Hot Coffee </Text>
+                        <List.Item> Regular </List.Item>
+                        <List.Item> Large </List.Item>
+                        <Text> ₱20.00 </Text>
+                    </List.Item>
+                    
+                    <div style={{ width: 240, marginLeft: 'auto', marginRight: 'auto' }}>
+                 <Image radius="md" src="" alt="Box with Chopsticks"/>
+                </div>
+                    <list.Item trigger ="hover" delay={500} control = {<Button> Add to Cart </Button>}> 
+                        <Text weight={500}> Coke </Text>
+                        <List.Item> Regular </List.Item>
+                        <List.Item> Large </List.Item>
+                        <Text> ₱20.00 </Text>
+                    </List.Item>
+                    
+                    <div style={{ width: 240, marginLeft: 'auto', marginRight: 'auto' }}>
+                 <Image radius="md" src="" alt="Box with Chopsticks"/>
+                </div>
+                    <list.Item trigger ="hover" delay={500} control = {<Button> Add to Cart </Button>}> 
+                        <Text weight={500}> Pineapple Juice </Text>
+                        <List.Item> Regular </List.Item>
+                        <List.Item> Large </List.Item>
+                        <Text> ₱50.00 </Text>
+                    </List.Item>
+                    
+                    <div style={{ width: 240, marginLeft: 'auto', marginRight: 'auto' }}>
+                 <Image radius="md" src="" alt="Box with Chopsticks"/>
+                </div>
+                    <list.Item trigger ="hover" delay={500} control = {<Button> Add to Cart </Button>}> 
+                        <Text weight={500}> Chopsticks </Text>
+                        <Text> ₱15.00 </Text>
+                    </List.Item>
+                
+                    <div style={{ width: 240, marginLeft: 'auto', marginRight: 'auto' }}>
+                 <Image radius="md" src="" alt="Box with Chopsticks"/>
+                </div>
+                    <list.Item trigger ="hover" delay={500} control = {<Button> Add to Cart </Button>}> 
+                        <Text weight={500}> Extra Bowl </Text>
+                        <Text> ₱15.00 </Text>
+                    </List.Item>
+                    
+                    <div style={{ width: 240, marginLeft: 'auto', marginRight: 'auto' }}>
+                 <Image radius="md" src="" alt="Box with Chopsticks"/>
+                </div>
+                    <list.Item trigger ="hover" delay={500} control = {<Button> Add to Cart </Button>}> 
+                        <Text weight={500}> Miso Soup </Text>
+                        <Text> ₱250.00 </Text>
+                    </List.Item>
+                    
             </Container> 
         </>
     )
