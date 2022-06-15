a878ad1b14bbdacd23328034274ba63f609b675c
Karmotrine <yuanrcurena@gmail.com>
2022-06-15 20:57:58 +0800

    Updated Menu Page, made cards responsive and added category dividers

--- pages/menu/index.tsx
+++ pages/menu/index.tsx
@@ -1,4 +1,4 @@
-import { Container, Text, Divider, createStyles, Center, Card, Image, SimpleGrid } from "@mantine/core";
+import { Container, Text, Divider, createStyles, Center, Card, Image, SimpleGrid, Paper, Space } from "@mantine/core";
 import productTestData from "../../product_testdata.json"
 import productTestData2 from "../../product_testdata2.json"
 const useStyles = createStyles((theme) => ({
@@ -10,7 +10,6 @@ const useStyles = createStyles((theme) => ({
 
 export default function Menu() {
     const { classes } = useStyles()
-
     return(
         <>
             <Container py={48}>
@@ -18,57 +17,76 @@ export default function Menu() {
                     <Text className={classes.headerFont}>Menu</Text>
                 </Center>
                 <Divider />
-                <Text size="xl">Ramen Kits</Text>
-                <SimpleGrid cols={3}>
-                    {productTestData.map((product) => {
-                        return (
-                            <>
-                                {/* Add props: component="a", href="{dynamic product link}", target="_blank" */}
-                                <Card
-                                    shadow="sm"
-                                    p="xl"
-                                >
-                                    <Card.Section>
-                                        <Center>
-                                            <Image src={product.img} height={200} width={300} fit="cover" alt={`${product.name}_image`} />
-                                        </Center>
-                                    </Card.Section>
+                <Space h="lg"/>
+                <SimpleGrid cols={1}>
+                    <Paper shadow="xs" p="md">
+                        <Divider label="Ramen Kits" size="xl" />
+                        <Space h="md" />
+                        <SimpleGrid 
+                            cols={3}
+                            breakpoints={[
+                                { maxWidth: 980, cols: 3, spacing: 'md' },
+                                { maxWidth: 755, cols: 2, spacing: 'sm' },
+                                { maxWidth: 600, cols: 1, spacing: 'sm' },
+                              ]}
+                        >
+                            {productTestData.map((product) => {
+                                return (
+                                        /* Add props: component="a", href="{dynamic product link}", target="_blank" */
+                                        <Card
+                                            shadow="sm"
+                                            p="xl"
+                                            key={product.name}
+                                        >
+                                            <Card.Section>
+                                                <Center>
+                                                    <Image src={product.img} height={200} width={300} fit="cover" alt={`${product.name}_image`} />
+                                                </Center>
+                                            </Card.Section>
 
-                                    <Text size="lg">{product.name}</Text>
+                                            <Text size="lg">{product.name}</Text>
 
-                                    <Text size="xs">{product.desc}</Text>
-                                    <Text size="md">{`₱${product.price}`}</Text>
-                                </Card>
-                            </>
-                        )
-                    })}
+                                            <Text size="xs">{product.desc}</Text>
+                                            <Text size="md">{`₱${product.price}`}</Text>
+                                        </Card>
+                                )
+                            })}
+                        </SimpleGrid>
+                    </Paper>
+                    <Paper shadow="xs" p="md">
+                    <Divider label="Bento Kits" size="xl" />
+                    <Space h="md" />
+                        <SimpleGrid
+                            cols={3}
+                            breakpoints={[
+                                { maxWidth: 980, cols: 3, spacing: 'md' },
+                                { maxWidth: 755, cols: 2, spacing: 'sm' },
+                                { maxWidth: 600, cols: 1, spacing: 'sm' },
+                              ]}
+                        >
+                            {productTestData2.map((product) => {
+                                return (
+                                        <Card
+                                            shadow="sm"
+                                            p="xl"
+                                            key={product.name}
+                                        >
+                                            <Card.Section>
+                                                <Center>
+                                                    <Image src={product.img} height={200} width={300} fit="cover" alt={`${product.name}_image`} />
+                                                </Center>
+                                            </Card.Section>
+
+                                            <Text size="lg">{product.name}</Text>
+
+                                            <Text size="xs">{product.desc}</Text>
+                                            <Text size="md">{`₱${product.price}`}</Text>
+                                        </Card>
+                                )
+                            })}
+                        </SimpleGrid>
+                    </Paper>
                 </SimpleGrid>
-                <Text>Bento kits</Text>
-                <SimpleGrid cols={3}>
-                    {productTestData2.map((product) => {
-                        return (
-                            <>
-                                {/* Add props: component="a", href="{dynamic product link}", target="_blank" */}
-                                <Card
-                                    shadow="sm"
-                                    p="xl"
-                                >
-                                    <Card.Section>
-                                        <Center>
-                                            <Image src={product.img} height={200} width={300} fit="cover" alt={`${product.name}_image`} />
-                                        </Center>
-                                    </Card.Section>
-
-                                    <Text size="lg">{product.name}</Text>
-
-                                    <Text size="xs">{product.desc}</Text>
-                                    <Text size="md">{`₱${product.price}`}</Text>
-                                </Card>
-                            </>
-                        )
-                    })}
-                </SimpleGrid>
-
             </Container> 
         </>
     )
