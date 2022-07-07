18fb7081c816bbb4ffe02fcefc6ad243aa73dd48
Karmotrine <yuanrcurena@gmail.com>
2022-07-07 23:31:22 +0800

    Testing loading product #13

--- pages/product/[productId].tsx
+++ pages/product/[productId].tsx
@@ -6,17 +6,11 @@ export default function Orders() {
     const router = useRouter();
     const slugId = router.query.productId
     const productTest = useProduct(slugId.toString())
-    const { name, id, description } = productTest.data
-    const { price, type } = productTest.data.metadata
-    console.log(`${name} ${id} ${description}`)
-    console.log(`${price} ${type}`)
+    console.log(productTest)
     return (
         <Container py={48}>
-            { 1 && <>{name}</>}
-            { 1 && <>{id}</>}
-            { 1 && <>{description}</>}
-            { 1 && <>{price}</>}
-            { 1 && <>{type}</>}
+            Test {slugId}
+            
         </Container> 
     )
 }
