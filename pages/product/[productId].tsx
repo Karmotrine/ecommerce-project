a1e7f4a5c43bc3a4bb8243db5d2890586c4fa8c0
Karmotrine <yuanrcurena@gmail.com>
2022-07-08 09:24:44 +0800

    Testing loading product #14

--- pages/product/[productId].tsx
+++ pages/product/[productId].tsx
@@ -3,16 +3,25 @@ import { useRouter } from "next/router"
 import { useProduct } from "../../lib/hooks/useProduct"
 import { doc, getDoc } from "firebase/firestore"
 import { firestore } from "../../lib/firebaseClient"
+import { useState } from "react"
 
 export default function Orders() {
     const router = useRouter();
     const slugId = router.query.productId as string
-    const docRef = doc(firestore, "products", slugId)
-    const productTest = async () => await getDoc(docRef);
+    const [name, setName] = useState("empty")
+    const productTest = async () => {
+        const docRef = doc(firestore, "products", slugId)
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+            setName(docSnap.data.name)
+        } else {
+            alert("Document does not exists.")
+        }
+    }
 
     return (
         <Container py={48}>
-            Test : {productTest.toString}
+            Test : {name}
         </Container> 
     )
 }
