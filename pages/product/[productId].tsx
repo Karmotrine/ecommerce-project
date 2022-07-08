b978da83a0fc9c0cb2e066dc1f91250754549452
Karmotrine <yuanrcurena@gmail.com>
2022-07-08 15:01:46 +0800

    Product page, Review page testing #2

--- pages/product/[productId].tsx
+++ pages/product/[productId].tsx
@@ -21,6 +21,9 @@ export default function Orders() {
     if (product.data == undefined) {
         return (<NotFoundTitle />)
     }
+    const price = parseInt(product.data.metadata.price)
+    const discountFactor = parseInt(product.data.metadata.discount) / 100
+    const discountedPrice = price * discountFactor
     return(
         <Container py={48}>
             <Grid columns={35}>
@@ -28,16 +31,31 @@ export default function Orders() {
                     <Image
                         radius="xl"
                         alt="product image"
-                        src={product.data.images[0]}
+                        src={product.data.images[0] as string}
                     />
                 </Grid.Col>
                 <Grid.Col span={15}>
                     <Stack justify="flex-start">
                         <Text>{product.data.name}</Text>
                         <Text size="xs">{product.data.metadata.type}</Text>
-                        <Text size="xs">{product.data.metadata.stockLeft} Stocks Left</Text>
-                        <Text size="lg">₱{product.data.metadata.price}</Text>
-                        <Text>{product.data.description}</Text>
+                        <Text size="xs">{product.data.metadata.stockLeft.toString()} Stocks Left</Text>
+                            {product.data.metadata.discount == "0" ?
+                            <Text size="lg">
+                                ₱{product.data.metadata.price}
+                            </Text> :
+                            <>
+                                <Text size="lg" color="red">
+                                    ₱<s>{product.data.metadata.price} </s>
+                                </Text>
+                                <Text size="lg" weight={500}>{discountedPrice}</Text>
+                                <Text size="sm">{product.data.metadata.discount}% off</Text>
+                            </>
+                            }
+                        {
+                        product.data.description ? 
+                        <Text>{product.data.description}</Text> : 
+                        <Text color="grey">No description</Text>
+                        }
                         <QuantityIncrementer stockLeft={product.data.metadata.stockLeft}/>
                     </Stack>
                 </Grid.Col>
