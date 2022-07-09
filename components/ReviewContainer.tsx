646b17f673b7017680f8b30a3c2040095ad7b52c
Karmotrine <yuanrcurena@gmail.com>
2022-07-09 20:39:20 +0800

    Fix #4 Edit Review Function

--- components/ReviewContainer.tsx
+++ components/ReviewContainer.tsx
@@ -12,44 +12,46 @@ interface ReviewContainerProps {
 
 export default function ReviewContainer(ReviewContainerProps) {
     const user = useUser()
-    const userReview = useProductReview(ReviewContainerProps.productId!, user.data!.uid)
     const [edit, setEdit] = useState(false)
+    const userReview = useProductReview(ReviewContainerProps.productId!, user.data?.uid)
     const userReviewData = userReview.data
-
-    if (!user.data) {
-        return (
-        <Container py={15}>
-            <Center>Please Log-in to write a review.</Center>
-        </Container>
-        )
-    }
-
+    
     return (
         <>
-            {!edit && userReview.status === 'success' && !!userReview && (
+            {!user.data ? (
+                <Container py={15}>
+                    <Center>Please Log-in to write a review.</Center>
+                </Container>
+            ):
+            (
                 <>
-                    <ReviewCard {...userReviewData!} />
-                    <Button 
-                        fullWidth={true}
-                        color="red"
-                        onClick={() => setEdit(true)}
-                    >
-                        Edit Review
-                    </Button>
+                    {!edit && userReview.status === 'success' && !!userReview && (
+                        <>
+                            <ReviewCard {...userReviewData!} />
+                            <Button 
+                                fullWidth={true}
+                                color="red"
+                                onClick={() => setEdit(true)}
+                            >
+                                Edit Review
+                            </Button>
+                        </>
+                    )}
+                    {userReview.status === "success" && !userReviewData && (
+                        <ReviewForm productId={ReviewContainerProps.productId}/>
+                    )}
+                    {!!edit && !!userReviewData && (
+                        <>
+                            <ReviewForm
+                                productId={ReviewContainerProps.productId}
+                                initialMessage={userReviewData.message}
+                                initialRating={userReviewData.rating}
+                            />
+                        </>
+                    )}
                 </>
-            )}
-            {userReview.status === "success" && !userReviewData && (
-                <ReviewForm productId={ReviewContainerProps.productId}/>
-            )}
-            {!!edit && !!userReviewData && (
-                <>
-                    <ReviewForm
-                        productId={ReviewContainerProps.productId}
-                        initialMessage={userReviewData.message}
-                        initialRating={userReviewData.rating}
-                    />
-                </>
-            )}
+            )
+            }
         </>
     )
 }
