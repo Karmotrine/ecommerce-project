68ab63dae5134d95817294bede16d72469d84c05
Karmotrine <yuanrcurena@gmail.com>
2022-07-19 09:03:18 +0800

    Revert log-out

--- components/ui/UiNavbar.tsx
+++ components/ui/UiNavbar.tsx
@@ -17,7 +17,6 @@ import Image from "next/image"
 import { useState } from "react"
 import OrderModalForm from "../OrderModalForm"
 import useOrderModal from "../hooks/useOrderModal"
-import { NextResponse } from 'next/server'
 
 const NAVLINKS = [
     {
@@ -45,7 +44,7 @@ export default function UiNavbar() {
     const signOut = useAuthSignOut(auth, {
         onSuccess(){
             localStorage.clear()
-            NextResponse.redirect('/')
+            router.push("/")
             showNotification(LOGOUT_NOTIFICATION)
         },
     })
